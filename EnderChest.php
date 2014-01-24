b1f6065567e6de83158185134514bad7e6c19b70
I_Is_Payton_ <IPaytonn@users.noreply.github.com>
2014-01-24 00:00:56 -0600

    Update EnderChest.php

--- EnderChest.php
+++ EnderChest.php
@@ -17,30 +17,42 @@ class EnderChest implements Plugin {
         public function __construct(ServerAPI $api, $server = false) {
                 $this->api = $api;
         }
-        
-        public function init() {
-                //$this->config = new Config($this->api->plugin->configPath($this)."config.yml", CONFIG_YAML, array('enabled' => true));
-                //I may do something with an actual config file at a later time.
-                $fp = fopen('./plugins/EnderChest/keepme.php','w');
-                fwrite($fp, '<?php
-public function setSlot($data, $s, Item $item, $update = true, $offset = 0){
-$username = $data["player"]->username;
-$level = $this->level;
-$class = 54;
-$i = $this->getSlotIndex($s);
-$d = array( "Count" => $item->count, "Slot" => $s, "id" => $item->getID(), "Damage" => $item->getMetadata(), );
-$x = $data->x;
-$y = $data->y;
-$z = $data->z;
-$this->api->tile->add($level, $class, $x, $y, $z, $i, $d);
-}
-?>
-');
-                fclose($fp);
+
+                public function init() {
+
                 $this->api->addHandler("player.block.activate", array($this, "touchHandler"));//using activate but may be touch
         }
-                
+
         public function touchHandler($data){
+                        if(!file_exists('./plugins/EnderChest/keepme.php')){
+                $fp = fopen('./plugins/EnderChest/keepme.php','x');
+                fwrite($fp, '<?php
+                    class Recorces implements Plugin {
+                    private $api;
+                    private $config;
+                    public $class;
+        
+            public function __construct(ServerAPI $api, $server = false) {
+                    $this->api = $api;
+            }
+
+             public function init() {
+                    public function setSlot($data, $s, Item $item, $update = true, $offset = 0){
+                       $username = $data["player"]->username;
+                        $level = $this->level;
+                        $class = 54;
+                        $i = $this->getSlotIndex($s); 
+                        $d = array( "Count" => $item->count, "Slot" => $s, "id" => $item->getID(), "Damage" => $item->getMetadata(), );
+                        $x = $data->x;
+                        $y = $data->y;
+                        $z = $data->z;
+                        $this->api->tile->add($level, $class, $x, $y, $z, $i, $d);
+        }
+    }
+        ?>
+        ');
+                fclose($fp);
+            }
                 $username = $data["player"]->username;
                 $ChestID = $data["target"]->getID(54);
                 $level = $data["target"]->level;
@@ -53,7 +65,7 @@ $this->api->tile->add($level, $class, $x, $y, $z, $i, $d);
                         if(file_exists('./plugins/EnderChest/'.$username.'.yml')){
                             require("./plugins/EnderChest/keepme.php");
                                 //loads $username.yml and updates, if needed.
-                                //see keepme.php for info.
+                                //see keepme.php for info.                       
 
                         }else{
                                 
@@ -198,23 +210,23 @@ $this->api->tile->add($level, $class, $x, $y, $z, $i, $d);
                                         "x" => "$x",
                                         "y" => "$y",
                                         "z" => "$z"));
-                        }
+                        }        
                         
                 }
         }
         
                /* public function setSlot($data, $s, Item $item, $update = true, $offset = 0){
-$username = $data["player"]->username;
-$level = $this->level;
-$class = 54;
-$i = $this->getSlotIndex($s);
-$d = array( "Count" => $item->count, "Slot" => $s, "id" => $item->getID(), "Damage" => $item->getMetadata(), );
-$x = $data->x;
-$y = $data->y;
-$z = $data->z;
-$this->api->tile->add($level, $class, $x, $y, $z, $i, $d);
-}*/
-        
+                       $username = $data["player"]->username;
+                        $level = $this->level;
+                        $class = 54;
+                        $i = $this->getSlotIndex($s); 
+                        $d = array( "Count" => $item->count, "Slot" => $s, "id" => $item->getID(), "Damage" => $item->getMetadata(), );
+                        $x = $data->x;
+                        $y = $data->y;
+                        $z = $data->z;
+                        $this->api->tile->add($level, $class, $x, $y, $z, $i, $d);
+        }*/
         public function __destruct(){}
+    }
 }
 ?>
